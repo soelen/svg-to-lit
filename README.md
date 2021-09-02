55737f7fe1d5c7c09839ada5a0fb439d91ec6bf2
Uwe Sölen Flaig <uwe.soelen.flaig@gmail.com>
2021-09-03 00:33:10 +0200

    Better readme, added new param and keywords.

--- README.md
+++ README.md
@@ -1,8 +1,31 @@
 # svg-to-lit
 Converting svg files for lit.dev based projects.
 
+This package will generate a js or ts file (whatever you specify as output). It will use the *file name* to export all transpiled svg string in camelized *variable names*.
+
+## Example
+
+```
+svg-tolit --input './icons/*' --output './dist/icons.js'
+```
+May generate a file like this: 
+```
+// ./dist/icons.js
+
+import { svg } from 'lit';
+
+export const arrow = svg`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" version="1.2">...</svg>`
+
+export const happyBunny = svg`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" version="1.2">...</svg>`
+
+// etc.
+```
+
 ## Params
 
-- -i | --input:  Specify input. [Glob](https://www.npmjs.com/package/glob) patterns are possible.
-- -o | --output: Specify output.
-- -h | --help:   Call this fancy help once again.
+```
+-i | --input:  Specify input. [Glob](https://www.npmjs.com/package/glob) patterns are possible. This parameter is required.
+-o | --output: Specify output. This parameter is required.
+-h | --help:   Call this fancy help once again.
+-c | --compress: Compress / Optimize svg in the process.
+```
--- cli.js
+++ cli.js
@@ -10,11 +10,14 @@ import { constantify, camelize } from 'inflected';
 const [,, ...args] = process.argv;
 
 const help = args.findIndex( arg => arg === '--help' || arg === '-h' );
+const compressIndex = args.findIndex( arg => arg === '--compress' || arg === '-c' );
 
 const exitWithSomeHelp = () => {
-	console.log( '-i | --input:  Specify input. Glob patterns are possible.' )
-	console.log( '-o | --output: Specify output.' )
-	console.log( '-h | --help:   Call this fancy help once again.' )
+	console.log( '-i | --input:  Specify input. Glob patterns are possible. This parameter is required.' )
+	console.log( '-o | --output: Specify output. This parameter is required.' )
+	console.log( '-c | --compress: Compress / Optimize svg in the process.' )
+	console.log( '-h | --help: Call this fancy help once again.' )
+
 	process.exit();
 }
 
@@ -33,7 +36,9 @@ glob( args[ inputIndex ], ( error, files ) => {
 	.map( file => ({ path: file, buffer: readFileSync( file ) }) )
 	.map( data => ({ path: data.path, svg: data.buffer.toString()}))
 	.map( data => {
-		return optimize( data.svg, { multipass: true, path: data.path } )
+		return compressIndex > 0
+		? optimize( data.svg, { multipass: true, path: data.path } )
+		: { data: data.svg, path: data.path };
 	} );
 
 	writeFileSync( args[ outputIndex ], `import { svg } from 'lit';
